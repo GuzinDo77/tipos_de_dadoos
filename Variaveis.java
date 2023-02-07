b95d3b4017543ce19fc8d9135be267ecb54f461b
Guza xt <gustavomom1227@gmail.com>
2023-02-07 15:15:36 +0000

    aulao

--- Variaveis.java
+++ Variaveis.java
@@ -1,3 +1,14 @@
 public class Variaveis{
 
-}
+    public static void main(String[] args){
+
+    int idade=20;
+    byte quantidadeBola=5;
+    char letra='M';
+    boolean vivo=false;
+    double salario = 1340.50;
+    float salarioMenor = 1200.00f;
+    short alunosPalmital = 10000;
+    long populacaoPalmital = 22000;
+    system.out.printIn(idade);
+}}
