9cfc2205af10492e43ccbedbc4c5751676ec3d44
Guza xt <gustavomom1227@gmail.com>
2023-02-15 17:11:41 +0000

    Repeticao

--- EstruturaDeRepeticao.java
+++ EstruturaDeRepeticao.java
@@ -1,3 +1,35 @@
-public class EstruturaDeRepeticao{
+public class EstruturaDeRepeticao {
+    public static void main(String[] args) {
+        /*
+         * for(int i=150; i<=300; i++){
+         * System.out.println(i);
+         * }
+         */
 
+            //------------------------------------//
+
+        /*int soma = 0;
+        for (int i = 1; i <= 1000; i++) {
+            soma = soma + i;
+        }
+        System.out.println(soma);*/
+
+        //------------------------------------//
+
+        /*for(int i=3; i<=100; i+=3){
+            System.out.println(i);
+    }*/
+
+        //------------------------------------//
+
+        int fatResult = 0;
+        for(int i=0;i>1;i--){
+            if(i ==0){
+                fatResult = 1;
+                break;
+            }else{
+                fatResult = i * fatResult;
+            }
+        }
+}
 }
